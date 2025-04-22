14662802a92f3221cac23ebd5a911f8555a5fa5b
Ethan.OToole <eotoole005@gmail.com>
2025-04-22 15:16:23 -0400

    More formatting changes

--- PayrollCalculator/src/main/java/com/pluralsight/PayrollCalcApp.java
+++ PayrollCalculator/src/main/java/com/pluralsight/PayrollCalcApp.java
@@ -1,4 +1,5 @@
 package com.pluralsight;
+
 import java.io.*;
 
 public class PayrollCalcApp {
@@ -12,28 +13,28 @@ public class PayrollCalcApp {
             //Contains the entire line that will be reader by our BufferedReader object.
             String input;
 
-            while((input = reader.readLine()) != null) {
+            while ((input = reader.readLine()) != null) {
                 //Used to skip the first line containing the id | name | ..."
-                if(input.startsWith("id")) {
+                if (input.startsWith("id")) {
                     continue;
                 }
                 //Checks through each info for an employee the id name ...
                 String[] employeeInfo = input.split("\\|");
-                    for(int i = 0; i < employeeInfo.length; i++) {
-                        //Send each employee into an array of employees.
-                        employees[i] = employee;
-                        employee.setEmployeeId(Short.parseShort(employeeInfo[i]));
-                        employee.setName(employeeInfo[i + 1]);
-                        employee.setHoursWorked(Float.parseFloat(employeeInfo[i + 2]));
-                        employee.setPayRate(Float.parseFloat(employeeInfo[i + 3]));
-                        System.out.printf("ID: " + "%d |" + " " + "Name: " + "%s |" + " Pay: $%.2f%n" , employee.getEmployeeId(), employee.getName(), employee.getGrossPay(employee.getHoursWorked(), employee.getPayRate()));
-                        //Break when we are done displaying the information for each employee.
-                        break;
-                    }
+                for (int i = 0; i < employeeInfo.length; i++) {
+                    //Send each employee into an array of employees.
+                    employees[i] = employee;
+                    employee.setEmployeeId(Short.parseShort(employeeInfo[i]));
+                    employee.setName(employeeInfo[i + 1]);
+                    employee.setHoursWorked(Float.parseFloat(employeeInfo[i + 2]));
+                    employee.setPayRate(Float.parseFloat(employeeInfo[i + 3]));
+                    System.out.printf("ID: " + "%d |" + " " + "Name: " + "%s |" + " Pay: $%.2f%n", employee.getEmployeeId(), employee.getName(), employee.getGrossPay(employee.getHoursWorked(), employee.getPayRate()));
+                    //Break when we are done displaying the information for each employee.
+                    break;
+                }
             }
             //Close the reader when we are done looking inside the employees.csv
             reader.close();
-        }catch(IOException e) {
+        } catch (IOException e) {
             System.out.println("Error reading file");
             System.out.println(e.getMessage());
         }
