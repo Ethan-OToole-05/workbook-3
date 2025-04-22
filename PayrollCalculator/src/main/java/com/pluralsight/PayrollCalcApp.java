ea366e44339fac190a897d86a28225e1ab6cd0de
Ethan.OToole <eotoole005@gmail.com>
2025-04-22 13:52:26 -0400

    Able to display each employees info and store each employee in an employees array

--- PayrollCalculator/src/main/java/com/pluralsight/PayrollCalcApp.java
+++ PayrollCalculator/src/main/java/com/pluralsight/PayrollCalcApp.java
@@ -4,15 +4,12 @@ import java.io.*;
 public class PayrollCalcApp {
     public static void main(String[] args) {
         try {
+            //Initialize our array, variable, file reader, and our buffered reader.
             FileReader file = new FileReader("employees.csv");
             BufferedReader reader = new BufferedReader(file);
-
-            short employeeId, counter = 0;
-            String name;
-            float hoursWorked, payRate;
             Employee[] employees = new Employee[10];
             Employee employee = new Employee();
-
+            //Contains the entire line that will be reader by our BufferedReader object.
             String input;
 
             while((input = reader.readLine()) != null) {
@@ -20,26 +17,25 @@ public class PayrollCalcApp {
                 if(input.startsWith("id")) {
                     continue;
                 }
+                //Checks through each info for an employee the id name ...
                 String[] employeeInfo = input.split("\\|");
-//                while(counter <= employees.length) {
                     for(int i = 0; i < employeeInfo.length; i++) {
+                        //Send each employee into an array of employees.
                         employees[i] = employee;
                         employee.setEmployeeId(Short.parseShort(employeeInfo[i]));
                         employee.setName(employeeInfo[i + 1]);
                         employee.setHoursWorked(Float.parseFloat(employeeInfo[i + 2]));
                         employee.setPayRate(Float.parseFloat(employeeInfo[i + 3]));
                         System.out.println(employee.getEmployeeId() + " " + employee.getName() + " $" + employee.getGrossPay(employee.getHoursWorked(), employee.getPayRate()));
+                        //Break when we are done displaying the information for each employee.
                         break;
                     }
-
-//                }
             }
+            //Close the reader when we are done looking inside the employees.csv
             reader.close();
         }catch(IOException e) {
             System.out.println("Error reading file");
             System.out.println(e.getMessage());
         }
-
-
     }
 }
