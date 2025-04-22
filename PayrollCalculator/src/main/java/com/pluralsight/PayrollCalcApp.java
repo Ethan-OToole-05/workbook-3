8e345a674b4a3fb4b964f6fe6125e62c18231d07
Ethan.OToole <eotoole005@gmail.com>
2025-04-22 11:58:38 -0400

    Set up loop going through employee info, able to skip the first line

--- PayrollCalculator/src/main/java/com/pluralsight/PayrollCalcApp.java
+++ PayrollCalculator/src/main/java/com/pluralsight/PayrollCalcApp.java
@@ -1,17 +1,39 @@
 package com.pluralsight;
 import java.io.*;
-import java.util.Scanner;
 
 public class PayrollCalcApp {
     public static void main(String[] args) {
         try {
-            FileReader file = new FileReader("PayrollCalculator/employees.csv");
+            FileReader file = new FileReader("employees.csv");
             BufferedReader reader = new BufferedReader(file);
+
+            short employeeId, counter = 0;
+            String name;
+            float hoursWorked, payRate;
+            Employee[] employees = new Employee[10];
+            Employee employee;
 
             String input;
 
             while((input = reader.readLine()) != null) {
-                System.out.println(input);
+                //Used to skip the first line containing the id | name | ..."
+                if(input.startsWith("id")) {
+                    continue;
+                }
+                String[] employeeInfo = input.split("\\|");
+                while(counter <= employees.length) {
+                    for(int i = 0; i < employeeInfo.length; i++) {
+                        employee = new Employee();
+                        employee.setEmployeeId(Short.parseShort(employeeInfo[i]));
+                        employee.setName(employeeInfo[i + 1]);
+                        employee.setHoursWorked(Float.parseFloat(employeeInfo[i + 2]));
+                        employee.setPayRate(Float.parseFloat(employeeInfo[i + 3]));
+                        System.out.println(employee.getEmployeeId() + employee.getName() + employee.getGrossPay(employee.getHoursWorked(), employee.getPayRate()));
+                        continue;
+                    }
+                    counter++;
+                }
+
             }
             reader.close();
         }catch(IOException e) {
