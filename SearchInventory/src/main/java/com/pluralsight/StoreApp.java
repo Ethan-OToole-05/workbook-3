66d73c4cff596e20f89b4fb4c6ba1d2742da47f5
Ethan.OToole <eotoole005@gmail.com>
2025-04-24 11:42:28 -0400

    Collection.sort issue only does lists not array lists?

--- SearchInventory/src/main/java/com/pluralsight/StoreApp.java
+++ SearchInventory/src/main/java/com/pluralsight/StoreApp.java
@@ -1,19 +1,23 @@
 package com.pluralsight;
 
+import javax.naming.Name;
 import java.util.ArrayList;
 import java.io.*;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class StoreApp {
     public static void main(String[] args) {
         ArrayList<Product> inventory = getInventory();
 
-
         System.out.println("This is the following inventory available: ");
-        for(Product product : inventory) {
+        for (Product product : inventory) {
             System.out.println("ID: " + product.getId() + " Name: " + product.getName() + " Price: $" + product.getPrice());
         }
     }
-    public static ArrayList<Product> getInventory(){
+
+    public static ArrayList<Product> getInventory() {
         ArrayList<Product> inventory = new ArrayList<Product>();
         Product product = new Product();
         try {
@@ -25,7 +29,7 @@ public class StoreApp {
 //            inventory.add(new Product(1334, "Computer", 199.99f));
 //            inventory.add(new Product(1223, "Golf Clubs", 99.99f));
             String input;
-            while((input = reader.readLine()) != null) {
+            while ((input = reader.readLine()) != null) {
                 String[] items = input.split("\\|");
                 int id = Integer.parseInt(items[0]);
                 String name = items[1];
@@ -34,10 +38,10 @@ public class StoreApp {
                 inventory.add(new Product(id, name, price));
 
             }
-        }catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
-
+        Collections.sort(inventory);
 
         return inventory;
     }
